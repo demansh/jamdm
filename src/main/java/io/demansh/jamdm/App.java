a037dd68ec84b2342264c7bfd1b45da38338c6c5
demansh <demansh17@gmail.com>
2021-01-02 12:21:34 +0500

    mvn generated

---
+++ src/main/java/io/demansh/jamdm/App.java
@@ -0,0 +1,7 @@
+package io.demansh.jamdm;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/io/demansh/jamdm/AppTest.java
@@ -0,0 +1,12 @@
+package io.demansh.jamdm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
